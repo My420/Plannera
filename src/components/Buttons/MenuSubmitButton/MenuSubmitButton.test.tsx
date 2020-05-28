398f6f2c2cf3e8ff48aeebff67d3e6799fc3db3c
My420 <my420V@gmail.com>
2020-05-28 18:52:11 +0300

    components.MenuSubmitButton/test/fix: change Enzyme to React Testing Library

--- src/components/Buttons/MenuSubmitButton/MenuSubmitButton.test.tsx
+++ src/components/Buttons/MenuSubmitButton/MenuSubmitButton.test.tsx
@@ -1,75 +1,53 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import MenuSubmitButton from './MenuSubmitButton';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MenuSubmitButton, { MenuSubmitButtonProps } from './MenuSubmitButton';
 
-describe('Test MenuSubmitButton component', () => {
-  const text = 'Text Button';
-  const name = 'testBtn';
-  const testAttr = 'test';
+describe('test MenuSubmitButton component', () => {
   const mockFn = jest.fn();
-  const wrapper = shallow(
-    <MenuSubmitButton text={text} name={name} onButtonClick={mockFn} data-test={testAttr} />,
-  );
+  const props: MenuSubmitButtonProps = {
+    text: 'Test Button',
+    name: 'MSButton',
+    onButtonClick: mockFn,
+  };
+
+  const expected = {
+    type: 'submit',
+  };
+
+  afterEach(cleanup);
 
   test('should match snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = render(<MenuSubmitButton {...props} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  test('should render  tag button', () => {
-    const tag = wrapper.find('button');
-    expect(tag).toHaveLength(1);
+  test('should render tag button', () => {
+    render(<MenuSubmitButton {...props} />);
+    const elem = screen.getByText(props.text);
+    expect(elem.tagName).toBe('BUTTON');
   });
 
-  test('tag button should have correct default className', () => {
-    const actual = wrapper.find('button').prop('className');
-    const expected = 'button ';
-    expect(actual).toBe(expected);
+  test('button should have correct attr', () => {
+    render(<MenuSubmitButton {...props} />);
+    const elem = screen.getByText(props.text);
+    expect(elem).toHaveAttribute('type', expected.type);
+    expect(elem).toHaveAttribute('name', props.name);
   });
 
-  test('tag button should have correct attr type', () => {
-    const actual = wrapper.find('button').prop('type');
-    const expected = 'submit';
-    expect(actual).toBe(expected);
+  test('the button should receive focus by pressing the tab key', () => {
+    render(<MenuSubmitButton {...props} />);
+    const elem = screen.getByText(props.text);
+    expect(document.body).toHaveFocus();
+    userEvent.tab();
+    expect(elem).toHaveFocus();
   });
 
-  test('tag button should have correct attr name', () => {
-    const actual = wrapper.find('button').prop('name');
-    const expected = name;
-    expect(actual).toBe(expected);
-  });
-
-  test('tag button should have correct attr onClick', () => {
-    const actual = wrapper.find('button').prop('onClick');
-    const expected = mockFn;
-    expect(actual).toEqual(expected);
-  });
-
-  test('tag button should have correct data-test attr', () => {
-    const actual = wrapper.find('button').prop('data-test');
-    const expected = testAttr;
-    expect(actual).toEqual(expected);
-  });
-
-  test('tag button should have correct children text', () => {
-    const actual = wrapper.find('button').prop('children');
-    const expected = text;
-    expect(actual).toEqual(expected);
-  });
-
-  test('tag button should have correct className attr if extensionClass prop was transferred', () => {
-    const extClass = 'newClass';
-    const props = {
-      text,
-      name,
-      onClick: mockFn,
-      'data-test': testAttr,
-      extensionClass: extClass,
-    };
-    const actual = wrapper
-      .setProps(props)
-      .find('button')
-      .prop('className');
-    const expected = `button ${extClass}`;
-    expect(actual).toEqual(expected);
+  test('click on button should call the passed function', () => {
+    render(<MenuSubmitButton {...props} />);
+    const elem = screen.getByText(props.text);
+    expect(mockFn).not.toHaveBeenCalled();
+    userEvent.click(elem);
+    expect(mockFn).toHaveBeenCalledTimes(1);
   });
 });
