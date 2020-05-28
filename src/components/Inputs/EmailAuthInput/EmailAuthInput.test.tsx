9cd3eb23c0978d8d0b7a06a4257e3362c4487c09
My420 <my420V@gmail.com>
2020-05-28 16:47:21 +0300

    components.Inputs/test/fix: change Enzyme to React Testing Library for components test

--- src/components/Inputs/EmailAuthInput/EmailAuthInput.test.tsx
+++ src/components/Inputs/EmailAuthInput/EmailAuthInput.test.tsx
@@ -1,33 +1,72 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { AuthInputProps } from '../AuthInput/AuthInput';
-import { EMAIL } from '../../../utils/constant';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import EmailAuthInput, { EmailAuthInputProps } from './EmailAuthInput';
+import { EMAIL } from '../../../utils/constant';
 
 describe('test EmailAuthInput component', () => {
+  const mockFn = jest.fn();
   const props: EmailAuthInputProps = {
     value: '',
     isValid: false,
     error: '',
-    onInputChange: () => {},
-    extensionContainerClass: 'testContainerClass',
-    extensionInputClass: 'testInputClass',
+    onInputChange: mockFn,
   };
-  const wrapper = shallow(<EmailAuthInput {...props} />);
+
+  const expected = {
+    textLabel: 'Email',
+    placeholder: 'Email',
+    type: 'email',
+    name: EMAIL,
+  };
+
+  afterEach(cleanup);
 
   test('should match snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = render(<EmailAuthInput {...props} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  test('should render Memo(AuthInput) component with expected props', () => {
-    const expectedProps: AuthInputProps = {
-      id: 'email',
-      name: EMAIL,
-      type: 'email',
-      placeholder: 'Email',
-      label: 'Email',
-      ...props,
-    };
-    expect(wrapper.find('Memo(AuthInput)').props()).toEqual(expectedProps);
+  test('should render label that associate with input', () => {
+    render(<EmailAuthInput {...props} />);
+    const elem = screen.getByLabelText(expected.textLabel);
+    expect(elem.tagName).toBe('INPUT');
+  });
+
+  test("should render tag span with label text and 'visually-hidden' class", () => {
+    render(<EmailAuthInput {...props} />);
+    const elem = screen.getByText(expected.textLabel);
+    expect(elem).toHaveClass('visually-hidden');
+  });
+
+  test('should render tag input with correct attr', () => {
+    render(<EmailAuthInput {...props} />);
+    const elem = screen.getByPlaceholderText(expected.placeholder);
+    expect(elem.tagName).toBe('INPUT');
+    expect(elem).toHaveValue(props.value);
+    expect(elem).toHaveAttribute('type', expected.type);
+  });
+
+  test('should render correct tag span with error if props.isValid = false', () => {
+    render(<EmailAuthInput {...props} />);
+    const elem = screen.getByTestId('error');
+    expect(elem).toHaveClass('error');
+    expect(elem).toHaveTextContent(props.error);
+  });
+
+  test('should render correct tag span with error if props.isValid = true', () => {
+    const errorText = 'error error';
+    const newProps = { ...props, isValid: true, error: errorText };
+    render(<EmailAuthInput {...newProps} />);
+    const elem = screen.getByText(errorText);
+    expect(elem).toHaveClass('valid');
+  });
+
+  test('when user enters text should call props.onInputChange function', async () => {
+    render(<EmailAuthInput {...props} />);
+    const elem = screen.getByPlaceholderText(expected.placeholder);
+    const text = 'test user input';
+    await userEvent.type(elem, text, { allAtOnce: true });
+    expect(mockFn).toHaveBeenCalledWith(expected.name, text);
   });
 });
--- src/components/Inputs/FirstNameAuthInput/FirstNameAuthInput.test.tsx
+++ src/components/Inputs/FirstNameAuthInput/FirstNameAuthInput.test.tsx
@@ -1,33 +1,72 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { AuthInputProps } from '../AuthInput/AuthInput';
-import { FIRST_NAME } from '../../../utils/constant';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import FirstNameAuthInput, { FirstNameAuthInputProps } from './FirstNameAuthInput';
+import { FIRST_NAME } from '../../../utils/constant';
 
 describe('test FirstNameAuthInput component', () => {
+  const mockFn = jest.fn();
   const props: FirstNameAuthInputProps = {
     value: '',
     isValid: false,
     error: '',
-    onInputChange: () => {},
-    extensionContainerClass: 'testContainerClass',
-    extensionInputClass: 'testInputClass',
+    onInputChange: mockFn,
   };
-  const wrapper = shallow(<FirstNameAuthInput {...props} />);
+
+  const expected = {
+    textLabel: 'First name',
+    placeholder: 'First name',
+    type: 'text',
+    name: FIRST_NAME,
+  };
+
+  afterEach(cleanup);
 
   test('should match snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = render(<FirstNameAuthInput {...props} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  test('should render Memo(AuthInput) component with expected props', () => {
-    const expectedProps: AuthInputProps = {
-      id: 'firstName',
-      name: FIRST_NAME,
-      type: 'text',
-      placeholder: 'First name',
-      label: 'First name',
-      ...props,
-    };
-    expect(wrapper.find('Memo(AuthInput)').props()).toEqual(expectedProps);
+  test('should render label that associate with input', () => {
+    render(<FirstNameAuthInput {...props} />);
+    const elem = screen.getByLabelText(expected.textLabel);
+    expect(elem.tagName).toBe('INPUT');
+  });
+
+  test("should render tag span with label text and 'visually-hidden' class", () => {
+    render(<FirstNameAuthInput {...props} />);
+    const elem = screen.getByText(expected.textLabel);
+    expect(elem).toHaveClass('visually-hidden');
+  });
+
+  test('should render tag input with correct attr', () => {
+    render(<FirstNameAuthInput {...props} />);
+    const elem = screen.getByPlaceholderText(expected.placeholder);
+    expect(elem.tagName).toBe('INPUT');
+    expect(elem).toHaveValue(props.value);
+    expect(elem).toHaveAttribute('type', expected.type);
+  });
+
+  test('should render correct tag span with error if props.isValid = false', () => {
+    render(<FirstNameAuthInput {...props} />);
+    const elem = screen.getByTestId('error');
+    expect(elem).toHaveClass('error');
+    expect(elem).toHaveTextContent(props.error);
+  });
+
+  test('should render correct tag span with error if props.isValid = true', () => {
+    const errorText = 'error error';
+    const newProps = { ...props, isValid: true, error: errorText };
+    render(<FirstNameAuthInput {...newProps} />);
+    const elem = screen.getByText(errorText);
+    expect(elem).toHaveClass('valid');
+  });
+
+  test('when user enters text should call props.onInputChange function', async () => {
+    render(<FirstNameAuthInput {...props} />);
+    const elem = screen.getByPlaceholderText(expected.placeholder);
+    const text = 'test user input';
+    await userEvent.type(elem, text, { allAtOnce: true });
+    expect(mockFn).toHaveBeenCalledWith(expected.name, text);
   });
 });
